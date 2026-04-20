f145a32e00ba40d7df8a188a83fa0f21aa7176fe
Nguyễn Quang Vinh <vinh@vinhMacAir.local>
2026-04-21 01:00:25 +0700

    feat(auth): add User model with password hashing middleware and comparePassword method

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
     {
@@ -37,6 +38,23 @@ const userSchema = new mongoose.Schema(
         timestamps: true
     }
 );
+
+// Hash password before saving to database
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next()
+});
+
+// Compare password when login 
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 
 const User = mongoose.model("User", userSchema);
 
